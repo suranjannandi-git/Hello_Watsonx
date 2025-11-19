8367da8c7350d50889ff66e7a498188fcb0ae60a
SURANJAN NANDI <suranjan_nandi@yahoo.com>
2025-11-19 09:45:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,25 +12,25 @@ cd "$SCRIPT_DIR"
 # 1. Create virtual environment IF NOT exists
 ##################################################
 if [ ! -d "venv-dev" ]; then
-    echo "[1/4] Creating Python virtual environment (venv-dev)..."
+    echo "[1/3] Creating Python virtual environment (venv-dev)..."
     python3.12 -m venv venv-dev
 else
     # echo "[1/4] Virtual environment already exists. Skipping creation."
-    echo -e "\e[33m[1/4] Virtual environment already exists. Skipping creation.\e[0m"
+    echo -e "\e[33m[1/3] Virtual environment already exists. Skipping creation.\e[0m"
 
 fi
 
 ##################################################
 # 2. Activate virtual environment
 ##################################################
-echo "[2/4] Activating virtual environment..."
+echo "[2/3] Activating virtual environment..."
 # shellcheck source=/dev/null
 source venv-dev/bin/activate || { echo "Failed to activate venv-dev"; exit 1; }
 
 ##################################################
 # 3. Install dependencies
 ##################################################
-echo "[4/4] Installing Python dependencies..."
+echo "[3/3] Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
